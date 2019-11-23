ba2166822501954a93cc0ddeac66d8f0be55f76d
DentonWarnock <dentonwarnock@gmail.com>
2019-11-22 18:36:00 -0800

    Reducer created for smurf loading.

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,33 @@
+import { SMURF_LOAD_START, SMURF_LOAD_START, SMURF_LOAD_FAILURE, SMURF_LOAD_SUCCESS } from '../actions';
+
+const initialState = {
+    isLoading: false,
+    error: "",
+    smurfs: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case SMURF_LOAD_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case SMURF_LOAD_SUCCESS: 
+            return {
+                ...state,
+                isLoading: false,
+                smurfs: [...smurfs, action.payload]
+            }
+        case SMURF_LOAD_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+};
+
+export default reducer;
